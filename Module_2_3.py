e185878c24166a779728a9c7bb82c5478c97f0de
Python1965 <python65@rambler.ru>
2024-08-08 15:56:33 +0300

    Add files via upload

--- Module_2_3.py
+++ Module_2_3.py
@@ -14,9 +14,6 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 ind = 0
-while ind < len(my_list) - 1:
-    if my_list[ind] < 0:
-        break
-
+while (ind < len(my_list)) and  (my_list[ind] >= 0):
     print(my_list[ind])
     ind += 1
